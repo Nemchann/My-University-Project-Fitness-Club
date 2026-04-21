5b0f94abc17b6057a8a85b788b1bdcdcc79083b4
Nemchann <bayaka79@mail.ru>
2026-04-21 19:49:32 +0300

    added UserService.java

--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/users/service/UserService.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/users/service/UserService.java
@@ -1,4 +1,20 @@
 package com.nemchann.fitnessbackend.users.service;
 
+import com.nemchann.fitnessbackend.users.repository.ProfileRepository;
+import com.nemchann.fitnessbackend.users.repository.RoleRepository;
+import com.nemchann.fitnessbackend.users.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private ProfileRepository profileRepository;
+    private RoleRepository roleRepository;
+    private UserRepository userRepository;
+
+    public UserService(ProfileRepository profileRepository, RoleRepository roleRepository,
+                       UserRepository userRepository){
+        this.profileRepository = profileRepository;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
 }
