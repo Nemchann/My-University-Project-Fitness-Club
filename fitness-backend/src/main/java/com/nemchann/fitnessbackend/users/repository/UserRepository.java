cdc27ef27046cf2cd36afdd4c776c2812444bf41
Nemchann <bayaka79@mail.ru>
2026-04-24 14:59:02 +0300

    added new custom method into UserRepository

--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/users/repository/UserRepository.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/users/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.nemchann.fitnessbackend.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByLogin(String login);
+
+    Optional<User> findByEmail(String email);
 }
