59eaf661b3e5485d9a8477d1ba201b3428339db8
Nemchann <bayaka79@mail.ru>
2026-04-26 23:10:05 +0300

    common packed was structured

--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/common/config/DatabaseConfiguration.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/common/config/DatabaseConfiguration.java
@@ -1,4 +1,4 @@
-package com.nemchann.fitnessbackend.common;
+package com.nemchann.fitnessbackend.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/common/config/SecurityConfig.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/common/config/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.nemchann.fitnessbackend.common;
+package com.nemchann.fitnessbackend.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //Тут додумать
