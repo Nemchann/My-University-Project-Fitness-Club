bb584702f456dc292cd3ef520b5460b04dbcf079
Nemchann <bayaka79@mail.ru>
2026-04-26 23:08:43 +0300

    added new UserController and two methods

---
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/users/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.nemchann.fitnessbackend.users.controller;
+
+import com.nemchann.fitnessbackend.users.dto.UserEditingDto;
+import com.nemchann.fitnessbackend.users.dto.UserRegistrationDto;
+import com.nemchann.fitnessbackend.users.dto.UserResponseDto;
+import com.nemchann.fitnessbackend.users.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/fitness-club/users")
+@RequiredArgsConstructor
+@Tag(name = "User Controller", description = "Управление пользователями и регистрация")
+public class UserController {
+    private final UserService service;
+
+    @PostMapping("/register")
+    @Operation(summary = "Создать пользователя")
+    public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserRegistrationDto userRegistrationDto){
+        UserResponseDto userResponseDto = service.createUser(userRegistrationDto);
+        return new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(summary = "Удалить пользователя")
+    public ResponseEntity<Void> delete(@Valid @RequestBody UserEditingDto userEditingDto){
+        service.deleteUser(userEditingDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
