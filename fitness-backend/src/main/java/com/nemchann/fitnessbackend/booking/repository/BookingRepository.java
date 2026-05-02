df2a4e4817bb867ca701edf710d29024d4fd4ce1
Nemchann <bayaka79@mail.ru>
2026-05-02 22:04:24 +0300

    changed id in Booking

--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/repository/BookingRepository.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/repository/BookingRepository.java
@@ -4,6 +4,8 @@ import com.nemchann.fitnessbackend.booking.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
+public interface BookingRepository extends JpaRepository<Booking, UUID> {
 }
