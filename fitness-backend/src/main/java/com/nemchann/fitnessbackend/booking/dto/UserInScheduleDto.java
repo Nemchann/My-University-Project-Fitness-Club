106e5dd6f302e6e0412fe2d90acb2def5e7c6140
Nemchann <bayaka79@mail.ru>
2026-05-03 13:37:57 +0300

    added new DTOs into Booking part

---
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/dto/UserInScheduleDto.java
@@ -0,0 +1,14 @@
+package com.nemchann.fitnessbackend.booking.dto;
+
+import lombok.Data;
+
+@Data
+public class UserInScheduleDto {
+
+    private String fullName;
+
+    private String phone;
+
+    private String email;
+
+}
