88391091e59d49b5b722cd0fe1b8f036a3b5aa1c
Nemchann <bayaka79@mail.ru>
2026-05-04 22:07:30 +0300

    added new nethod in repository

--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/schedule/repository/ScheduleRepository.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,7 @@
 package com.nemchann.fitnessbackend.schedule.repository;
 
 import com.nemchann.fitnessbackend.schedule.entity.Schedule;
+import com.nemchann.fitnessbackend.schedule.enums.WorkoutTypeEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -27,4 +28,11 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
             "AND s.currentParticipants < s.maxParticipants " +
             "AND s.startTime > :now")
     Page<Schedule> findAvailableSchedules(@Param("now") LocalDateTime now, Pageable pageable);
+
+    //Возможно додумать
+    @Query("SELECT s FROM Schedule s " +
+    "JOIN s.workout w " +
+    "JOIN w.workoutType wt " +
+    "HAVING wt = :workoutTypeEnum AND s.isActive = true")
+    Page<Schedule> findAvailableSchedulesByWorkoutType(@Param("workoutTypeEnum") WorkoutTypeEnum workoutTypeEnum);
 }
