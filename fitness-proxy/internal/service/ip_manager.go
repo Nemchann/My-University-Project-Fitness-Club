f0d56eb33720c662a4e3a652a879421375b5503e
Nemchann <bayaka79@mail.ru>
2026-05-08 13:15:00 +0300

    added ip_manager and tests

---
+++ fitness-proxy/internal/service/ip_manager.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"github.com/yl2chen/cidranger"
+	"net"
+	"fmt"
+)
+
+type IPManager struct {
+    whitelist cidranger.Ranger
+    blacklist cidranger.Ranger
+}
+
+func NewIPManager() *IPManager {
+    return &IPManager{
+        whitelist: cidranger.NewPCTrieRanger(),
+        blacklist: cidranger.NewPCTrieRanger(),
+    }
+}
+
+func (m *IPManager) IsAllowed(ip net.IP) (bool, string) {
+    // 1. Проверяем черный список (высший приоритет п. 1.2.1)
+    if contains, _ := m.blacklist.Contains(ip); contains {
+        return false, "blacklisted"
+    }
+
+    // 2. Проверяем белый список (п. 1.2.2)
+    if contains, _ := m.whitelist.Contains(ip); contains {
+        return true, "whitelisted"
+    }
+
+    // 3. Политика по умолчанию (п. 1.2.3)
+    return true, "default" 
+}
+
+func (m *IPManager) AddRule(network string, ruleType string) error{
+	
+    _, ipNet, err := net.ParseCIDR(network)
+    if err != nil {
+        return err
+    }
+
+    // 2. Создаем запись для рейнджера
+    entry := cidranger.NewBasicRangerEntry(*ipNet)
+
+    // 3. Распределяем по спискам в зависимости от типа
+    switch ruleType {
+    case "white":
+        return m.whitelist.Insert(entry)
+    case "black":
+        return m.blacklist.Insert(entry)
+    case "grey":
+        // Если решишь добавить логику для серых списков
+        return nil 
+    default:
+        return fmt.Errorf("unknown rule type: %s", ruleType)
+    }
+}
