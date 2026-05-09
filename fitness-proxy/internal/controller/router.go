8ea24c50e58aec1104889411ebabb0a0cf9ae117
Nemchann <bayaka79@mail.ru>
2026-05-09 23:46:16 +0300

    added new endpoint

--- fitness-proxy/internal/controller/router.go
+++ fitness-proxy/internal/controller/router.go
@@ -7,13 +7,15 @@ import (
 )
 
 // Потом вынести в main.go и добавить туда все эндпоинты управления (пока только /reload)
-func SetupRouter(ipRepo *repository.MongoIPRepo, ipManager *service.IPManager, proxyHandler gin.HandlerFunc) *gin.Engine {
+func SetupRouter(ipRepo *repository.MongoIPRepo, ipManager *service.IPManager, limiterManager *service.IPRateLimiter, proxyHandler gin.HandlerFunc) *gin.Engine {
     r := gin.Default()
 
     // Группа управления
     admin := r.Group("/management")
     {
         admin.GET("/reload", ReloadRulesHandler(ipRepo, ipManager))
+
+        admin.GET("/stats", GetStatsHandler(limiterManager))
         // Сюда добавим новые эндпоинты
     }
 
