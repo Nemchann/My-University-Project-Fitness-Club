53b2d8b4f8a3d6387615821e8dded69dd1ef80c2
Nemchann <bayaka79@mail.ru>
2026-05-12 18:21:30 +0300

    added endpoints that can be cached

--- fitness-proxy/internal/middleware/cache.go
+++ fitness-proxy/internal/middleware/cache.go
@@ -2,14 +2,33 @@ package middleware
 
 import (
 	"bytes"
-	"net/http"
 	"fitness-proxy/internal/service"
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
 type responseBodyWriter struct {
 	gin.ResponseWriter
 	body *bytes.Buffer
+}
+
+var cacheablePaths = []string{
+    "/api/fitness-club/users/get_all_clients",
+    "/api/fitness-club/schedules/get_schedules_by_week",
+	"/api/fitness-club/schedules/get_schedules_by_date",
+    "/api/fitness-club/bookings/past",
+}
+
+func isCacheable(path string) bool {
+    for _, p := range cacheablePaths {
+        if strings.HasPrefix(path, p) {
+            return true
+        }
+    }
+    return false
 }
 
 // Перехватываем метод Write, чтобы сохранить копию данных
@@ -21,7 +40,7 @@ func (w responseBodyWriter) Write(b []byte) (int, error) {
 func CacheMiddleware(cache *service.CacheManager) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 1. Кешируем только GET запросы
-		if c.Request.Method != http.MethodGet {
+		if c.Request.Method != http.MethodGet || !isCacheable(c.Request.URL.Path){
 			c.Next()
 			return
 		}
@@ -32,6 +51,7 @@ func CacheMiddleware(cache *service.CacheManager) gin.HandlerFunc {
 		// 2. Проверяем кеш
 		if data, found := cache.Get(key); found {
 			c.Data(http.StatusOK, "application/json", data)
+			fmt.Println("Из кеша")
 			c.Abort() // Дальше к Java не идем!
 			return
 		}
