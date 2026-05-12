83647b63543af41d4832ae69cad881eabb86c87e
Nemchann <bayaka79@mail.ru>
2026-05-12 18:24:57 +0300

    added new admin endpoints

--- fitness-proxy/internal/controller/router.go
+++ fitness-proxy/internal/controller/router.go
@@ -8,21 +8,24 @@ import (
 
 // Потом вынести в main.go и добавить туда все эндпоинты управления 
 func SetupRouter(ipRepo *repository.MongoIPRepo, ipManager *service.IPManager, 
-    limiterManager *service.IPRateLimiter, proxyHandler gin.HandlerFunc, cacheManager *service.CacheManager) *gin.Engine {
-    r := gin.Default()
-
+    limiterManager *service.IPRateLimiter, 
+    cacheManager *service.CacheManager, r *gin.Engine) *gin.RouterGroup {
+    
     // Группа управления
     admin := r.Group("/management")
     {
         admin.GET("/reload", ReloadRulesHandler(ipRepo, ipManager))
 
         admin.GET("/stats", GetStatsHandler(limiterManager))
+
+        admin.GET("/rules", GetAllRulesHandler(ipRepo))
         
         admin.DELETE("/cache", FlushCacheHandler(cacheManager)) // Новый метод для очистки кеша
+
+        admin.POST("/insert_rule", AddRuleHandler(ipRepo, ipManager))
+
+        admin.DELETE("/delete/:id", DeleteRuleHandler(ipRepo, ipManager))
     }
 
-    // Все остальное — в прокси
-    r.NoRoute(proxyHandler)
-
-    return r
+    return admin
 }
