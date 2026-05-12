1c852f60bb2bd1a4a0a77876513d13642faef741
Nemchann <bayaka79@mail.ru>
2026-05-12 18:23:37 +0300

    added new methods

--- fitness-proxy/internal/controller/ip_handler.go
+++ fitness-proxy/internal/controller/ip_handler.go
@@ -6,6 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AddRuleRequest struct {
+	IP   string `json:"network" binding:"required"`
+	Type string `json:"type" binding:"required"` // "black", "white", "grey"
+}
+
+func AddRuleHandler(ipRepo *repository.MongoIPRepo, ipManager *service.IPManager) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req AddRuleRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(400, gin.H{"error": "Неверный формат данных"})
+			return
+		}
+
+		// 1. Сохраняем в MongoDB
+		err := ipRepo.InsertRule(c.Request.Context(), req.IP, req.Type)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Ошибка сохранения в БД"})
+			return
+		}
+
+		// 2. Обновляем Radix Tree в памяти, чтобы изменения вступили в силу сразу
+		ipManager.UpdateRule(req.IP, req.Type)
+
+		c.JSON(200, gin.H{"status": "success", "message": "Правило добавлено"})
+	}
+}
+
 func ReloadRulesHandler(ipRepo *repository.MongoIPRepo, ipManager *service.IPManager) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 1. Снова лезем в базу за свежими правилами
@@ -22,5 +49,41 @@ func ReloadRulesHandler(ipRepo *repository.MongoIPRepo, ipManager *service.IPMan
 		}
 
 		c.JSON(200, gin.H{"status": "success", "loaded_rules": len(rules)})
+	}
+}
+
+// Получение всех правил
+func GetAllRulesHandler(ipRepo *repository.MongoIPRepo) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		rules, err := ipRepo.GetAll(c.Request.Context())
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Ошибка БД"})
+			return
+		}
+		c.JSON(200, rules)
+	}
+}
+
+// Удаление правила по ID
+func DeleteRuleHandler(ipRepo *repository.MongoIPRepo, ipManager *service.IPManager) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		err := ipRepo.DeleteByID(c.Request.Context(), id)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Не удалось удалить"})
+			return
+		}
+
+		rules, errAll := ipRepo.GetAll(c.Request.Context())
+
+
+		if errAll != nil {
+			c.JSON(500, gin.H{"error": "Не найти"})
+			return
+		}
+		
+		// После удаления из БД лучше обновить Radix Tree (вызвать Reload)
+		ipManager.Reload(rules) 
+		c.JSON(200, gin.H{"message": "Правило удалено"})
 	}
 }
--- fitness-proxy/internal/repository/ip_rules.go
+++ fitness-proxy/internal/repository/ip_rules.go
@@ -3,8 +3,12 @@ package repository
 import (
 	"context"
 	"fitness-proxy/internal/model"
+	"log"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    "fmt"
 )
 
 type MongoIPRepo struct {
@@ -30,4 +34,40 @@ func (r *MongoIPRepo) GetAll(ctx context.Context) ([]model.IPRule, error) {
         return nil, err
     }
     return rules, nil
+}
+
+func (r *MongoIPRepo) InsertRule(ctx context.Context, ip string, ruleType string) error {
+    doc := bson.M{
+        "ip":         ip,
+        "type":       ruleType,
+    }
+
+    result, error := r.collection.InsertOne(ctx, doc)
+    if error != nil {
+        return error
+    }
+    log.Printf("Inserted new IP rule with ID: %v", result.InsertedID)
+
+    return nil
+}
+
+
+func (r *MongoIPRepo) DeleteByID(ctx context.Context, id string) error {
+    // Превращаем строку в ObjectID
+    objID, err := primitive.ObjectIDFromHex(id)
+    if err != nil {
+        return err // Ошибка, если строка — не валидный hex-код ID
+    }
+
+    result, error := r.collection.DeleteOne(ctx, bson.M{"_id": objID})
+    if error != nil {
+        return error
+    }
+
+    if result.DeletedCount == 0 {
+        return fmt.Errorf("документ с id %s не найден", id)
+    }
+
+    log.Printf("Deleted IP rule with ID: %v", result.DeletedCount)
+    return nil
 }
