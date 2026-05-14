0654b608da53d4f1e46e1863a72522617a6c71bf
Nemchann <bayaka79@mail.ru>
2026-05-14 12:42:40 +0300

    added new methods un repositories

--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/repository/ClientSubscriptionRepository.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/repository/ClientSubscriptionRepository.java
@@ -1,7 +1,17 @@
 package com.nemchann.fitnessbackend.booking.repository;
 
 import com.nemchann.fitnessbackend.booking.entity.ClientSubscription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ClientSubscriptionRepository extends JpaRepository<ClientSubscription, Integer> {
+    Optional<ClientSubscription> findClientSubscriptionById(Integer id);
+
+    Page<ClientSubscription> findAllClientSubscriptions(UUID clientId, Pageable pageable);
+
+    //Сделать метод, который возвращает последний абонемент пользователя
 }
--- fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/repository/SubscriptionRepository.java
+++ fitness-backend/src/main/java/com/nemchann/fitnessbackend/booking/repository/SubscriptionRepository.java
@@ -4,9 +4,14 @@ import com.nemchann.fitnessbackend.booking.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
     Optional<Subscription> findBySubscriptionName(String subscriptionName);
+
+    List<Subscription> findAllSubscriptions();
+
+    Optional<Subscription> findSubscriptionById(Integer id);
 }
